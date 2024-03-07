65c95a930637f2add7603e7b104e32ac9d55436d
michelleresian <michellresian@gmail.com>
2024-03-08 02:45:49 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+
+    def match(self, words):
+        return [wrd for wrd in words if sorted(wrd.lower()) == sorted(self.word)]
+    
